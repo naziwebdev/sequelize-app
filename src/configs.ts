3b4fb17521cbbcc63546e445582fc917ddd0a3f1
nazi <nazipaeez777@gmail.com>
2024-09-25 20:48:07 +0330

    Create .env variables & organize in configs file

---
+++ src/configs.ts
@@ -0,0 +1,25 @@
+export default {
+    db: {
+      dbUser: process.env.DB_USER,
+      dbPassword: process.env.DB_PASSWORD,
+      dbHost:process.env.DB_HOST,
+      dbName:process.env.DB_NAME,
+      dbPort:process.env.DB_PORT,
+      dbDialect:process.env.DB_DIALECT,
+      poolSize: process.env.DB_POOL_SIZE,
+    },
+  
+    port: parseInt(process.env.PORT as any) || 4007,
+  
+    auth: {
+      accessTokenSecretKey: process.env.ACCESS_TOKEN_SECRET_KEY,
+      refreshTokenAccessKey: process.env.REFRESH_TOKEN_SECRET_KEY,
+      accessTokenExpireIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+      refreshTokenExpireIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    },
+    google:{},
+    redis:{},
+    domin:process.env.DOMIN,
+    isProduction : process.env.NODE_ENV === 'production' 
+  };
+  
