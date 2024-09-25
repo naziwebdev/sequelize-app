304f618e930e303e43ac518f2332942dae3abae3
nazi <nazipaeez777@gmail.com>
2024-09-26 00:22:33 +0330

    Create article model

---
+++ src/models/Article.ts
@@ -0,0 +1,60 @@
+import { DataTypes, Optional, Model } from "sequelize";
+import db from "../db";
+
+// in models dont put columns that have relation
+
+interface ArticleAttributes {
+  id: number;
+  title: string;
+  content: string;
+  slug: string;
+  cover: string;
+}
+
+interface ArticleCreationAttributes extends Optional<ArticleAttributes, "id"> {}
+
+interface ArticleInstance
+  extends Model<ArticleAttributes, ArticleCreationAttributes>,
+    ArticleAttributes {
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const Article = () => {
+  db.define<ArticleInstance>(
+    "article",
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      slug: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      cover: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "articles",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  );
+};
+
+export default Article;
