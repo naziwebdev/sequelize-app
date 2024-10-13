a06d231c04e0cf0176e0b595bcc755a823cba07a
nazi <nazipaeez777@gmail.com>
2024-10-13 20:14:19 +0330

    Develope create article api

--- controllers/article.js
+++ controllers/article.js
@@ -4,57 +4,7 @@ const articleSchema = require("../validators/articleSchema");
 
 exports.create = async (req, res, next) => {
   try {
-    // let { title, content, tags } = req.body;
-
-    // await articleSchema.validate(
-    //   { title, content, tags },
-    //   { abortEarly: false }
-    // );
-
-    // let slug = slugify(title, { lower: true });
-    // const copyOfSlug = slug;
-    // const author = req.user;
-
-    // tags = Array.isArray(tags) ? tags : [tags];
-
-    // tags: [] = tags.map((tag) => {
-    //   Tag.findOrCreate({
-    //     where: {
-    //       title: tag.trim(),
-    //     },
-    //   });
-    // });
-
-    // tags = await Promise.all(tags);
-
-    // let article;
-    // let i = 1;
-    // const coverPath = `images/covers/${req.file?.filename}`;
-    // while (!article) {
-    //   try {
-    //     article = await Article.create({
-    //       title,
-    //       content,
-    //       slug,
-    //       author_id: author.id,
-    //       cover: coverPath,
-    //     });
-
-    //     await article.addTag(tags.map((tag) => tag[0]));
-
-    //     return res.status(201).json({
-    //       ...article.dataValues,
-    //       tags: tags.map((tag) => tag[0].title),
-    //     });
-    //   } catch (err) {
-    //     if (err.original.code === "ER_DUP_ENTRY") {
-    //       slug = `${copyOfSlug}-${i++}`;
-    //     } else {
-    //       throw err;
-    //     }
-    //   }
-    // }
-    let { title, content, tags } = req.body; // frontend -> ["frontend"]
+    let { title, content, tags } = req.body;
     let slug = slugify(title, { lower: true });
     const copyOfSlug = slug;
     const authorId = req.user.id;
